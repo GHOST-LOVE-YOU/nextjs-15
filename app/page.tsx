f3fbbff69b0461c796168dcd264082c54decf22c
ghost-love-you <1756613825@qq.com>
2024-10-30 16:23:49 +0800

    test pull request again

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl text-violet-700 font-black">
-      Welcome to the world of Next.js!!!
+      Welcome to the world of Next.js!
     </h1>
   );
 }
