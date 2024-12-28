7149c343991e64d92488b821089cab1c3f1a700c
ghost-love-you <1756613825@qq.com>
2024-12-28 12:14:39 +0800

    set up eslint&prettier
    npm i --save-dev eslint-plugin-tailwindcss prettier
    eslint-plugin-tailwindcss
    
    a confusing stuff, when i have install tailwindcss lsp, i shoud install
    on my project again, claude explain it:
    "Lazyvim plugins are 'editor tools' for your personal use, packages
    installed in the project are 'project tools' for the entire project and
    team"

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ export default function Home() {
   console.log("Which component an I?");
 
   return (
-    <h1 className="font-bold text-3xl text-violet-700">
-      Welcome to the the world of nextjs
-    </h1>
+    <main>
+      <h1 className="text-3xl font-bold text-violet-700">
+        Welcome to the the world of nextjs
+      </h1>
+      <h1 className="text-3xl font-bold text-violet-700">
+        Welcome to the the world of nextjs
+      </h1>
+    </main>
   );
 }
